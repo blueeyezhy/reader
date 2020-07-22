83ade07b09106e7440e007deeffa170997ee39eb
Junhong <raimonfuns@gmail.com>
2020-07-22 10:45:39 +0800

    tweak(api-log): only log error message

--- server/models/api.js
+++ server/models/api.js
@@ -60,12 +60,13 @@ exports.errorHandler = async (ctx, next) => {
   try {
     await next();
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
     if (
       err.status &&
       err.status >= httpStatus.BAD_REQUEST &&
       err.status < httpStatus.INTERNAL_SERVER_ERROR
     ) {
+      console.log(err);
       ctx.throws(err);
       return;
     }
